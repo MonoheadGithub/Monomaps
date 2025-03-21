ca58b2af007f0151097a48429d6321139f09e296
Jet <rondellivey03@gmail.com>
2025-03-21 10:53:21 -0400

    Monoford: Sunset Island Init

---
+++ maps/Monoford/Layer1/map.js
@@ -0,0 +1,16 @@
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: 0,
+    maxZoom: 2
+}).setView([1024, 1024], 0);
+
+fetch('markers.json')
+    .then(response => response.json())
+    .then(data => {
+        const [width, height] = data.meta.map_resolution;
+        const imageBounds = [[0, 0], [height, width]];
+        L.imageOverlay('images/sunset-island-map.png', imageBounds).addTo(map);
+        map.fitBounds(imageBounds);
+        map.setMaxBounds(imageBounds);
+    })
+    .catch(error => console.error('Error fetching markers.json:', error));
